bffff46ed09144aaccba9003e59f1c6f769a8f1b
arcipeshane <arcipeshane@gmail.com>
2021-09-06 22:32:28 +0800

    added db

--- senior_citizen.sql
+++ senior_citizen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 06, 2021 at 02:21 PM
+-- Generation Time: Sep 06, 2021 at 04:31 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -24,11 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pension`
+--
+
+CREATE TABLE `pension` (
+  `pension_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `pension_name` varchar(50) NOT NULL,
+  `pension_amount` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pension`
+--
+
+INSERT INTO `pension` (`pension_id`, `user_id`, `pension_name`, `pension_amount`) VALUES
+(1, 1, 'Retired', '50000');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL,
   `user_name` varchar(150) NOT NULL,
   `user_age` varchar(150) NOT NULL,
   `user_phonenumber` varchar(150) NOT NULL
@@ -38,12 +60,19 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `user_name`, `user_age`, `user_phonenumber`) VALUES
-(1, 'Ray Laborte', '22', '09254567891');
+INSERT INTO `users` (`user_id`, `username`, `password`, `user_name`, `user_age`, `user_phonenumber`) VALUES
+(1, 'test456', 'test456', 'Ray Laborte 456', '22 456', '09254567891 456'),
+(2, 'test', 'test', '123', '123', '123');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `pension`
+--
+ALTER TABLE `pension`
+  ADD PRIMARY KEY (`pension_id`);
 
 --
 -- Indexes for table `users`
@@ -56,10 +85,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `pension`
+--
+ALTER TABLE `pension`
+  MODIFY `pension_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
